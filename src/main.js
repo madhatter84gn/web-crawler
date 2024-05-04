0c498b6d376cdf923951da7304a50ef8d779f0e4
madhatter84gn <madhatter84gn@fastmail.com>
2024-05-04 14:07:57 -0400

    complete main file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
+import { argv, exit } from 'node:process';
+
 function main() {
-  console.log('Hello World!!!')
+  if (argv.length < 3) {
+    console.log('To few arguments')
+    exit()
+  }
+
+  if (argv.length > 3) {
+    console.log('To many arguments')
+    exit()
+  }
+
+  console.log(`The webcrawler is starting with a baseURL of ${argv[2]}`)
 }
 
 main()
